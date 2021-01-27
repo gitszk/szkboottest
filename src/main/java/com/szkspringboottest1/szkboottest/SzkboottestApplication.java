65a8f278e7f9e8b9ce341b1fcc644e1d1fd66fb8
sunzhenkun <szksysu@163.com>
2021-01-27 15:37:09 +0800

    test@webservice

--- src/main/java/com/szkspringboottest1/szkboottest/SzkboottestApplication.java
+++ src/main/java/com/szkspringboottest1/szkboottest/SzkboottestApplication.java
@@ -3,7 +3,10 @@ package com.szkspringboottest1.szkboottest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.jws.WebService;
+
 @SpringBootApplication
+@WebService
 public class SzkboottestApplication {
 
 	public static void main(String[] args) {

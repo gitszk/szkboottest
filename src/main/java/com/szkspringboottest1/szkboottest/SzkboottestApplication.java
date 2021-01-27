0889c676e32d3b5e517ff840c5f0da18311d59fa
unknown <sunzhenkun@game.ntes>
2021-01-27 15:19:39 +0800

    Initial commit

---
+++ src/main/java/com/szkspringboottest1/szkboottest/SzkboottestApplication.java
@@ -0,0 +1,13 @@
+package com.szkspringboottest1.szkboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzkboottestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzkboottestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szkspringboottest1/szkboottest/SzkboottestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szkspringboottest1.szkboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzkboottestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
